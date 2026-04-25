82691948aff9ee437292f247fe9d0dc802d5aff7
sairam <reachsai8919@gmail.com>
2026-04-25 21:34:16 +0530

    April-25, 2026 evevning

--- rama.sh
+++ rama.sh
@@ -1,1 +1,7 @@
-Hi Rama 
+Hi Rama,
+
+How you doing?
+
+Today is April-15 of 2026.
+echo $date
+
--- sit.sh
+++ sit.sh
@@ -1,1 +1,6 @@
 Hi Seetha,
+
+I just created another branch in my local repository now I want to check it.
+
+
+Now I want to check
